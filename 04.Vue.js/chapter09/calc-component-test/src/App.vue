24daf4847fbde809cf8bfd91e53253e6feab26be
yuz <sky0jun8@naver.com>
2024-05-30 16:25:57 +0900

    20240530-09
    ch09
    reactive
    watch
    computed
    setup
    ..etc

--- 04.Vue.js/chapter09/calc-component-test/src/App.vue
+++ 04.Vue.js/chapter09/calc-component-test/src/App.vue
@@ -1,5 +1,22 @@
-<script setup></script>
+<template>
+  <div>
+    <Calc></Calc>
+  </div>
+</template>
 
-<template></template>
-
-<style scoped></style>
+<script>
+// 1. calc 실행하고 싶을땐
+// import Calc from "./components/Calc.vue";
+// 2. calc2 실행하고 싶을땐
+// import Calc from "./components/Calc2.vue";
+// 3. calc3 실행하고 싶을땐
+// import Calc from "./components/Calc3.vue";
+// 4. calc4 실행
+// import Calc from "./components/Calc4.vue";
+// 5. calc5 실행
+import Calc from "./components/Calc5.vue";
+export default {
+  name: "App",
+  components: { Calc },
+};
+</script>
